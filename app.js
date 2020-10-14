6c596c953c5e9744d746cfb9c762ffa15fbc3487
Jack2ee-dev <jack2ee.dev@gmail.com>
2020-10-14 23:47:57 +0900

    fix: mysql connection

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 
-const mysqlConnection = require('./databases/connection');
+const mysqlConnection = require('./databases/mysqlConnection');
 
 const app = express();
 
